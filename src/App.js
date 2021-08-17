122cfc14e85cd52f3c46b4d6e20e24551d546d9d
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-17 14:09:17 -0400

    Setup static data on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react';
+import { useFetch } from './helpers/useFetch';
+import Follower from './components/Follower';
+
 function App() {
-  return <div className='App'></div>;
+  const { loading, data } = useFetch();
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'>
+          {data.map(follower => {
+            return <Follower key={follower.id} {...data} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
